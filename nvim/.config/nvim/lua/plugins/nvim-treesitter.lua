0876b9f701d256d4e4af56c93f801bc15691d326
Matt Edwards <matthew.edwards@hpe.com>
2025-08-08 08:47:10 +0100

    Add incremental selection to treesitter

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -12,6 +12,13 @@ return {
                 enable = true,
             },
             indent = { enable = true },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    node_incremental = "v",
+                    node_decremental = "V",
+                },
+            },
         },
 
     },
