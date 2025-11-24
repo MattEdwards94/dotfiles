a24ec7c20dc07218140eedd60b46a5f0d2f98c56
Matt Edwards <matthew.edwards@hpe.com>
2025-11-24 10:11:46 +0000

    Change octo keymaps

--- nvim/.config/nvim/lua/plugins/octo.nvim.lua
+++ nvim/.config/nvim/lua/plugins/octo.nvim.lua
@@ -12,22 +12,22 @@ return {
         })
     end,
     keys = {
-        {"<leader>oil", ":Octo issue list<CR>", desc = "List issues"},
-        {"<leader>oip", ":Octo issue create<CR>", desc = "Create issue"},
-        {"<leader>oic", ":Octo issue close<CR>", desc = "Close issue"},
-        {"<leader>oid", ":Octo issue delete<CR>", desc = "Delete issue"},
-        {"<leader>ois", ":Octo issue search<CR>", desc = "Search issues"},
-        {"<leader>oib", ":Octo issue browser<CR>", desc = "Open issue in browser"},
+        {"<leader>goil", ":Octo issue list<CR>", desc = "List issues"},
+        {"<leader>goip", ":Octo issue create<CR>", desc = "Create issue"},
+        {"<leader>goic", ":Octo issue close<CR>", desc = "Close issue"},
+        {"<leader>goid", ":Octo issue delete<CR>", desc = "Delete issue"},
+        {"<leader>gois", ":Octo issue search<CR>", desc = "Search issues"},
+        {"<leader>goib", ":Octo issue browser<CR>", desc = "Open issue in browser"},
 
-        {"<leader>opl", ":Octo pr list<CR>", desc = "List PRs"},
-        {"<leader>opp", ":Octo pr create<CR>", desc = "Create PR"},
-        {"<leader>opc", ":Octo pr close<CR>", desc = "Close PR"},
-        {"<leader>opd", ":Octo pr delete<CR>", desc = "Delete PR"},
-        {"<leader>ops", ":Octo pr search<CR>", desc = "Search PRs"},
+        {"<leader>gopl", ":Octo pr list<CR>", desc = "List PRs"},
+        {"<leader>gopp", ":Octo pr create<CR>", desc = "Create PR"},
+        {"<leader>gopc", ":Octo pr close<CR>", desc = "Close PR"},
+        {"<leader>gopd", ":Octo pr delete<CR>", desc = "Delete PR"},
+        {"<leader>gops", ":Octo pr search<CR>", desc = "Search PRs"},
 
-        {"<leader>ors", ":Octo review start<CR>", desc = "Open review"},
-        {"<leader>orc", ":Octo review continue<CR>", desc = "Continue review"},
-        {"<leader>orx", ":Octo review discard<CR>", desc = "Discard review"},
-        {"<leader>orq", ":Octo review submit<CR>", desc = "Submit review"},
+        {"<leader>gors", ":Octo review start<CR>", desc = "Open review"},
+        {"<leader>gorc", ":Octo review continue<CR>", desc = "Continue review"},
+        {"<leader>gorx", ":Octo review discard<CR>", desc = "Discard review"},
+        {"<leader>gorq", ":Octo review submit<CR>", desc = "Submit review"},
     }
 }
